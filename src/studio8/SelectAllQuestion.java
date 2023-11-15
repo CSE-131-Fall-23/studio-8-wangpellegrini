a85fdac78d02c89a3755e1d77bd7cbecd708322c
Tang3672 <ijw91021@gmail.com>
2023-11-15 14:16:05 -0600

    Studio 8 Isaac wang

--- src/studio8/SelectAllQuestion.java
+++ src/studio8/SelectAllQuestion.java
@@ -5,11 +5,21 @@ public class SelectAllQuestion extends MultipleChoiceQuestion {
 	public SelectAllQuestion(String prompt, String answer, String[] choices) {
 		//Hint: 1 point per choice
 		//FIXME
+		super(prompt, answer, choices.length, choices);
+		
 	}
 	
 	public int checkAnswer(String givenAnswer) {
 		//FIXME Should return partial credit (if earned)!
-		return 0;
+		int points = super.getPoints();
+		for(int i = 0; i < givenAnswer.length(); i++) {
+			
+				if(super.getAnswer().indexOf(givenAnswer.charAt(i))==-1) {
+					points--;
+				}
+			
+		}
+		return points;
 	}
 	
 	public static void main(String[] args) {
